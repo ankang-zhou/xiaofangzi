dfd690ce8c26ac6564c8e2481d64ad153a0e1013
wanghao <17634318728@163.com>
2019-12-13 10:01:44 +0800

    修改了文章类

---
+++ smallHouse/src/main/java/cn/bdqn/mapper/ArticleMapper.java
@@ -0,0 +1,19 @@
+package cn.bdqn.mapper;
+
+import cn.bdqn.domain.Article;
+
+public interface ArticleMapper {
+    int deleteByPrimaryKey(Integer articleId);
+
+    int insert(Article record);
+
+    int insertSelective(Article record);
+
+    Article selectByPrimaryKey(Integer articleId);
+
+    int updateByPrimaryKeySelective(Article record);
+
+    int updateByPrimaryKeyWithBLOBs(Article record);
+
+    int updateByPrimaryKey(Article record);
+}
